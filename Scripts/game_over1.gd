3bf29870d6146b2cd469aa0267799e74a724e0e8
MoisesRMLM <moisesrmlm8282@gmail.com>
2026-02-25 20:39:33 +0100

    Scene connections and lvl1 finished
    TO DO: Finish connecting all the scenes and add music back in

--- Scripts/game_over1.gd
+++ Scripts/game_over1.gd
@@ -1,9 +1,7 @@
 extends Control
 
-
 func _on_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/level1.tscn")
-
+	get_tree().change_scene_to_file("res://Scenes/tutorial_lvl_1.tscn")
 
 func _on_button_2_pressed() -> void:
 	get_tree().quit()
--- Scripts/victory1.gd
+++ Scripts/victory1.gd
@@ -1,9 +1,7 @@
 extends Control
 
-
 func _on_play_again_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/level1.tscn")
-
+	get_tree().change_scene_to_file("res://Scenes/inbox.tscn")
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
